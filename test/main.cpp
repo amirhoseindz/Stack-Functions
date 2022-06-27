4c34d8233963a93c049f7aa68ad73b21f3911ee4
unknown <amir.mh17@yahoo.com>
2022-06-27 16:36:14 +0430

    put the functions in structur

--- test/main.cpp
+++ test/main.cpp
@@ -4,51 +4,51 @@ struct Stacks
 {
     int Stack[5];
     int Top;
+    static bool CheckStackEmptiness(int top)
+    {
+        if (top == -1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    int  *Push (int element)
+    {
+        Top = Top + 1;
+        Stack[Top] = element;
+        return Stack;
+    }
+    int Pop()
+    {
+        if (CheckStackEmptiness(Top))
+        {
+            cout << "underflow" << endl;
+            return -1;
+        }
+        else
+        {
+            Top = Top - 1;
+            return Stack [Top + 1];
+        }
+    }
 };
-bool CheckStackEmptiness(int top)
-{
-    if (top == -1)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-int  *Push (int Stack[5], int element, int &top)
-{
-    top = top + 1;
-    Stack[top] = element;
-    return Stack;
-}
-int Pop(int Stack[5], int &top)
-{
-    if (CheckStackEmptiness(top))
-    {
-        cout << "underflow" << endl;
-        return -1;
-    }
-    else
-    {
-        top = top - 1;
-        return Stack [top + 1];
-    }
-}
 int main()
 {
     Stacks S1;
     S1.Top = -1;
-    Push(S1.Stack, 1, S1.Top);
-    Push(S1.Stack, 2, S1.Top);
-    Push(S1.Stack, 3, S1.Top);
-    Push(S1.Stack, 4, S1.Top);
-    Push(S1.Stack, 5, S1.Top);
+    S1.Push(1);
+    S1.Push(2);
+    S1.Push(3);
+    S1.Push(4);
+    S1.Push(5);
     cout << "the arrays elements are : " << endl;
     for (int i : S1.Stack)
     {
         cout << i << ", ";
     }
     cout << endl;
-    cout << "the element that pops out is : " << Pop(S1.Stack, S1.Top) << endl;
+    cout << "the element that pops out is : " << S1.Pop() << endl;
 }
